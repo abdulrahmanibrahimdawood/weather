91f086a9d55f950f0ce0cbf0de0f21b6c25cee03
Abdulrahman Ibrahim <abdoibrahim9111@gmail.com>
2025-08-16 18:20:35 +0300

    create entity

---
+++ lib/core/utils/constances.dart
@@ -0,0 +1,3 @@
+class AppConstance {
+  static const String baseUrl = 'http://api.openweathermap.org/data/2.5';
+}
---
+++ lib/weather/domain/entites/weather.dart
@@ -0,0 +1,15 @@
+class Weather {
+  final int id;
+  final String main;
+  final String description;
+  final String cityName;
+  final int pressure;
+
+  Weather({
+    required this.id,
+    required this.main,
+    required this.description,
+    required this.cityName,
+    required this.pressure,
+  });
+}
